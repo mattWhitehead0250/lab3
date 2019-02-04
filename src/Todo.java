46c885037e4400e050ff938ae2298131a98e8e60
Matt Whitehead <matthew.s.whitehead-1@ou.edu>
2019-02-04 10:12:37 -0600

    Actually fixed this time

--- src/Todo.java
+++ src/Todo.java
@@ -5,14 +5,38 @@ OU ID_______________: 112737541
 OU EMAIL____________: matthew.s.whitehead-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Complete file reading
+2. Complete writeEquipment
+3. Complete writeStatistics
+4. Complete computeTotalWeight
+5. Complete getNamedEquipment
+6. Complete equipmentAboveCount
+7. Complete findMostExpensiveEquipment
+8. Complete Equipment constructor
+9. Complete toString
+10. Add getters to equipment
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 10
+2. 15
+3. 10
+4. 10
+5. 5
+6. 10
+7. 10
+8. 15
+9. 5
+10. 1
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 5
+2. 11
+3. 16
+4. 12
+5. 9
+6. 20
+7. 18
+8. 5
+9. 5
+10. 1
 */}
